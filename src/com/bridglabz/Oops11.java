d1ea172245a4e78489c9d81942c83cfe14bf83fc
vanibm15 <vanibm15@gmail.com>
2022-04-30 23:14:17 +0530

    added:deckofcard2

--- src/com/bridglabz/Oops11.java
+++ src/com/bridglabz/Oops11.java
@@ -3,36 +3,28 @@ package com.bridglabz;
 import java.util.Scanner;
 public class Oops11//stock
 {
-    String stockName;
-    int numberOfShares, sharePrice, totalValue;
-    public static void main(String[] args) {
-        int i1 = 10;
-        Oops11[] object = new Oops11[][ 10];
-        int i;
-        for (i = 0; i < 5; i++)
-            object[i].getData();
-        for (i = 0; i < 5; i++) {
-            object[i].calculate();
-            object[i].display();
+    int numberOfPlayers;
+
+    void Players() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n Enter the number of players in a game : ");
+        numberOfPlayers = scanner.nextInt();
+        if (numberOfPlayers < 2 && numberOfPlayers > 4) {
+            System.out.println("Number of players should be between 2 to 4.");
+            System.exit(0);
         }
     }
-    public void getData() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the stock information");
-        System.out.println(" Name              :     ");
-        stockName = scanner.next();
-        System.out.println(" Number Of Shares  :     ");
-        numberOfShares = scanner.nextInt();
-        System.out.println(" Share Price       :     ");
-        sharePrice = scanner.nextInt();
+
+
+
+    public static void main(String[] args) {
+        public void distribute; {
+            Oops11 cards = new Oops11();
+            cards.shuffle();
+            cards.displayCards();
+        }
     }
-    public void calculate() {
-        totalValue = sharePrice * numberOfShares;
-    }
-    public void display() {
-        System.out.println("Stock name          :   " + stockName);
-        System.out.println("Number of shares    :   " + numberOfShares);
-        System.out.println(" Share Price        :   " + sharePrice);
-        System.out.println(" Total value        :   " + totalValue);
+
+    private void shuffle() {
     }
 }
